39e26cefb14c3d4d775c7aa604ca826ec9324359
alexcermac <alex.alexandru1514@hotmail.com>
2023-12-06 09:14:21 +0200

    Project initialized

---
+++ src/main/java/com/project/ineedmaster/IneedmasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.ineedmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IneedmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IneedmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ineedmaster/IneedmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ineedmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IneedmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
